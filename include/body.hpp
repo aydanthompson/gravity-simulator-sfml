07aa3506454ad939940d6a8da3f187e063b927de
Aydan Thompson <aydan.thompson@bt.com>
2024-06-27 16:30:28 +0100

    Force calculation now part of Body class, bodies and shapes are now stored in dynamic arrays

--- include/body.hpp
+++ include/body.hpp
@@ -1,28 +1,36 @@
 #ifndef BODY_HPP
 #define BODY_HPP
 
+#include <string>
+
 class Body
 {
 public:
-    Body(long double x, long double y, long double mass, long double radius, long double vx, long double vy);
-    void update_rk4(float dt, long double fx, long double fy);
-    void update_e(float dt, long double fx, long double fy);
+    Body(const std::string& name, long double x, long double y, long double mass, long double radius, long double vx, long double vy);
+    void update_position_rk4(float dt, long double fx, long double fy);
+    void update_position_e(float dt);
+    void update_force(const Body &body_alt);
     long double getX() const;
     long double getY() const;
     long double getVX() const;
     long double getVY() const;
     long double getMass() const;
     long double getRadius() const;
+    long double getFX() const;
+    long double getFY() const;
+    std::string get_name() const;
+    void reset_force();
 
 private:
+    std::string name;
     long double x;
     long double y;
     long double mass;
     long double radius;
     long double vx;
     long double vy;
+    long double fx;
+    long double fy;
 };
-
-void calculateForce(const Body &body1, const Body &body2, long double &fx, long double &fy);
 
 #endif // BODY_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,49 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <iomanip>
+#include <vector>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1080, 1080), "Gravity Simulator");
-    window.setFramerateLimit(0);
+    // Window parameters
+    const int WINDOW_X = 1080;
+    const int WINDOW_Y = 1080;
 
     // Define scaling factors
-    const float distanceScaleFactor = 0.000001;
-    const float sizeScaleFactor = 0.00001;
+    const float DISTANCE_SCALE_FACTOR = 0.0000005;
+    const float SIZE_SCALE_FACTOR = 0.000005;
+
+    sf::RenderWindow window(sf::VideoMode(WINDOW_X, WINDOW_Y), "Gravity Simulator");
+    window.setFramerateLimit(60);
 
     // Body creation
-    Body earth(0, 0, 5.974e24, 6378.1e3, 0, 0);
-    Body moon(0.4055e9, 0, 0.07346e24, 1738.1e3, 0, -0.970e3);
+    Body body_a("Earth", 0, 0, 5.974e24, 6378.1e3, 0, 0);
+    Body body_b("Moon", 0.4055e9, 0, 0.07346e24, 1738.1e3, 0, -0.970e3);
+    Body body_c("Red", 0.8055e9, 0, 0.07346e4, 5738.1e2, 0, -0.470e3);
+    Body body_d("Magenta", 0, 0.8e9, 0.07346e8, 1738.1e3, 0.470e3, 0);
+    Body body_e("Yellow", 0, -0.8e9, 0.07346e8, 1738.1e3, -0.470e3, 0);
+    Body body_f("Green", 0.4055e9, -0.4055e9, 0.07346e8, 1738.1e3, 0, -0.970e3);
+
+    std::vector<Body> bodies = {body_a, body_b, body_c, body_d, body_e, body_f};
+    std::vector<sf::CircleShape> shapes = {};
+    std::vector<sf::Color> colours = {sf::Color::Blue, sf::Color::White, sf::Color::Red, sf::Color::Magenta, sf::Color::Yellow, sf::Color::Green};
+
+    // Views
+    sf::View defaultView(sf::FloatRect(-float(WINDOW_X) / 2.f, -float(WINDOW_Y) / 2.f, float(WINDOW_X), float(WINDOW_Y)));
+    sf::View yellowView(sf::FloatRect((-float(WINDOW_X) / 2.f) - (bodies[4].getX() * DISTANCE_SCALE_FACTOR), (-float(WINDOW_Y) / 2.f) - (bodies[4].getY() * DISTANCE_SCALE_FACTOR), float(WINDOW_X), float(WINDOW_Y)));
+    window.setView(defaultView);
 
     // Shape creation
-    sf::CircleShape earthShape(earth.getRadius() * sizeScaleFactor);
-    earthShape.setFillColor(sf::Color::Blue);
-    earthShape.setOrigin(earth.getRadius() * sizeScaleFactor, earth.getRadius() * sizeScaleFactor);
-    sf::CircleShape moonShape(moon.getRadius() * sizeScaleFactor);
-    moonShape.setFillColor(sf::Color::White);
-    moonShape.setOrigin(moon.getRadius() * sizeScaleFactor, moon.getRadius() * sizeScaleFactor);
+    for (int i = 0; i < bodies.size(); ++i)
+    {
+        sf::CircleShape shape(bodies[i].getRadius() * SIZE_SCALE_FACTOR);
+        shape.setFillColor(colours[i]);
+        shape.setOrigin(bodies[i].getRadius() * SIZE_SCALE_FACTOR, bodies[i].getRadius() * SIZE_SCALE_FACTOR);
+        shapes.insert(shapes.end(), shape);
+    }
 
-    float dt = 1.0;
+    float dt = 1440.0f;
 
-    long double mfx, mfy, efx, efy;
     float hours = 0;
 
     while (window.isOpen())
@@ -38,27 +56,39 @@ int main()
                 window.close();
         }
 
-        calculateForce(earth, moon, efx, efy);
-        // earth.update(dt, efx, efy);
-        calculateForce(moon, earth, mfx, mfy);
-        moon.update_e(dt, mfx, mfy);
+        for (Body &body_1 : bodies)
+        {
+            body_1.reset_force();
+            for (Body &body_2 : bodies)
+            {
+                if (&body_2 != &body_1)
+                {
+                    body_1.update_force(body_2);
+                }
+            }
+        }
+
+        for (Body &body_1 : bodies)
+        {
+            body_1.update_position_e(dt);
+        }
 
         hours += dt / (60 * 60);
 
-        std::cout << std::scientific << std::setprecision(1) << std::showpos;
-        std::cout << "d: Earth (" << earth.getX() << ", " << earth.getY() << "), Moon (" << moon.getX() << ", " << moon.getY() << ")\n";
-        std::cout << "v: Earth (" << earth.getVX() << ", " << earth.getVY() << "), Moon (" << moon.getVX() << ", " << moon.getVY() << ")\n";
-        std::cout << "f: Earth (" << efx << ", " << efy << "), Moon (" << mfx << ", " << mfy << ")\n";
-        std::cout << std::fixed << std::noshowpos;
-        std::cout << "Hours: " << hours << "\n";
-
-        earthShape.setPosition(earth.getX() * distanceScaleFactor, -earth.getY() * distanceScaleFactor);
-        moonShape.setPosition(moon.getX() * distanceScaleFactor, -moon.getY() * distanceScaleFactor);
+        // std::cout << std::scientific << std::setprecision(1) << std::showpos;
+        // std::cout << "d: Earth (" << earth.getX() << ", " << earth.getY() << "), Moon (" << moon.getX() << ", " << moon.getY() << ")\n";
+        // std::cout << "v: Earth (" << earth.getVX() << ", " << earth.getVY() << "), Moon (" << moon.getVX() << ", " << moon.getVY() << ")\n";
+        // std::cout << "f: Earth (" << earth.getFX() << ", " << earth.getFY() << "), Moon (" << moon.getFX() << ", " << moon.getFY() << ")\n";
+        // std::cout << std::fixed << std::noshowpos;
+        // std::cout << "Hours: " << hours << "\n";
 
         window.clear();
 
-        window.draw(earthShape);
-        window.draw(moonShape);
+        for (int i = 0; i < shapes.size(); ++i)
+        {
+            shapes[i].setPosition(bodies[i].getX() * DISTANCE_SCALE_FACTOR, -bodies[i].getY() * DISTANCE_SCALE_FACTOR);
+            window.draw(shapes[i]);
+        }
 
         window.display();
     }
